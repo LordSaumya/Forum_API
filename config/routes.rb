15d5a63e109480b08ef0e166b2bc8977e217878b
LordSaumya <e1036312@u.nus.edu>
2022-12-21 00:50:58 +0530

    Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #Routes
   resources :users, only: [:index, :show, :create, :update, :destroy]
   resources :forum_threads, only: [:index, :show, :create, :update, :destroy] do
     resources :comments, only: [:index, :show, :create, :update, :destroy]
